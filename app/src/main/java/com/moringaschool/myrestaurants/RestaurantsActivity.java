d7e45aac595667e92312507f7027536c07cf592e
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-24 22:20:35 +0300

    second commit

---
+++ app/src/main/java/com/moringaschool/myrestaurants/RestaurantsActivity.java
@@ -0,0 +1,23 @@
+package com.moringaschool.myrestaurants;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RestaurantsActivity extends AppCompatActivity {
+    public static final String TAG = RestaurantsActivity.class.getSimpleName();
+    private TextView mLocationTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
+    }
+}
