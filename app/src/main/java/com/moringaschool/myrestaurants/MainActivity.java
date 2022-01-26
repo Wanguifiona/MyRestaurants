328015f31b49a944c8fdba770a08c96115899cf9
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-26 21:00:31 +0300

    third commit

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -4,38 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView;
 
 
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.findRestaurantsButton)
+    Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
-          mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
-              @Override
-              public void onClick(View v){
-                  String location = mLocationEditText.getText().toString();
-
-                  Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                  intent.putExtra("location", location);
-                  startActivity(intent);
+        ButterKnife.bind(this);
 
 
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
 
 
-              }
-          });
+        }
     }
 }
+
